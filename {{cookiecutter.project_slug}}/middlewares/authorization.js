3b7faca0f7c562f5ec5502187760a0649a49286e
khan <khangds218@gmail.com>
2019-01-02 14:11:38 +0700

    Send json when unauthorized

--- {{cookiecutter.project_slug}}/middlewares/authorization.js
+++ {{cookiecutter.project_slug}}/middlewares/authorization.js
@@ -1,5 +1,19 @@
-var passport		= require('passport');
+let passport = require('passport');
+let sendJSONResponse = function (res, status, content) {
+  res.status(status);
+  res.json(content);
+};
 
-exports.authenticate = function() {
-	return passport.authenticate('jwt', { session: false });
+exports.authenticate = function (req, res, next) {
+  passport.authenticate('jwt', { session: false }, (err, token) => {
+    // console.log(token)
+    if (err || !token) {
+      return sendJSONResponse(res, 401, {
+        success: false,
+        message: 'Unauthorized'
+      })
+    }
+    req.user = token
+    next()
+  })(req, res, next);
 }
