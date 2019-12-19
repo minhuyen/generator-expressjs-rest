45c5e11b7360c7a4607408d61c2e3858ecd78428
Uyen Do <minhuyendo@gmail.com>
2019-12-19 10:41:22 +0700

    update auth and user api

--- generators/app/templates/backend/src/helpers/schemas.js
+++ generators/app/templates/backend/src/helpers/schemas.js
@@ -6,3 +6,20 @@ export const ObjectId = Joi.string().regex(/^[0-9a-fA-F]{24}$/);
 export const objectIdSchema = Joi.object({
   id: ObjectId.required()
 });
+
+export const paginateValidationSchema = Joi.object({
+  sort: Joi.string()
+    .default('-createdAt')
+    .optional(),
+  page: Joi.number()
+    .greater(0)
+    .default(1)
+    .positive()
+    .optional(),
+  limit: Joi.number()
+    .greater(0)
+    .default(25)
+    .positive()
+    .optional(),
+  filter: Joi.string().optional()
+});
