1fc842d33572db2b47ce0eb11870565181ddb6a7
khan <khangds218@gmail.com>
2020-11-04 10:12:36 +0700

    Add required func

--- generators/app/templates/backend/src/helpers/utils.js
+++ generators/app/templates/backend/src/helpers/utils.js
@@ -1,3 +1,6 @@
+import moment from 'moment'
+import jwttoken from 'jsonwebtoken'
+
 export const randomInt = (low, high) => {
   return Math.floor(Math.random() * (high - low) + low);
 };
@@ -8,4 +11,16 @@ export const randomVerfiedCode = () => {
 
 export const toNumber = (string) => {
   return Number(string) || string === '0' ? Number(string) : string
+}
+
+export const asyncForEach = async (array, callback) => {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
+
+export const parseMilisecond = ms => moment(parseInt(ms))
+
+export const decodeToken = function (token) {
+  return jwttoken.decode(token)
 }
