dde4c0338b64d7fbccffd3a5c538e799a249f969
khan <khangds218@gmail.com>
2020-11-04 10:35:43 +0700

    Add change password, update me, get me methods

--- generators/app/templates/backend/src/api/users/user.validation.js
+++ generators/app/templates/backend/src/api/users/user.validation.js
@@ -5,3 +5,16 @@ const { paginateValidationSchema } = schemas;
 
 export const paginateUserValidateSchema = paginateValidationSchema;
 
+export const changePasswordSchema = Joi.object({
+  currentPassword: Joi.string()
+    .min(4)
+    .max(255)
+    .default(" ")
+    .optional(),
+  newPassword: Joi.string()
+    .required()
+    .invalid(Joi.ref('password')),
+  confirmNewPassword: Joi.string()
+    .required()
+    .valid(Joi.ref('newPassword'))
+});
--- generators/app/templates/backend/src/api/users/users.service.js
+++ generators/app/templates/backend/src/api/users/users.service.js
@@ -6,6 +6,25 @@ class UserService extends Service {
   constructor(model) {
     super(model);
   }
+
+  async handleChangePassword(user, currentPassword, newPassword) {
+    const verifyPassword = user.toJSON().hasPassword ? user.verifyPassword(currentPassword) : 'social'
+
+    if (!verifyPassword) {
+      throw new Error('Incorrect Password')
+    } else {
+      user.password = newPassword;
+      return user.save();
+    }
+  }
+
+  async handleUpdateMe(userId, data) {
+    return await User.findByIdAndUpdate(userId, data, { new: true })
+  }
+
+  async handleGetMe(user) {
+    return { ...user.toJSON() }
+  }
 }
 
 export default new UserService(User);
