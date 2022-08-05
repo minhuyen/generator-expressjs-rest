83ca2c3426c8900f3c21ec02f74ee24b4a9ab936
Uyen Do <minhuyendo@gmail.com>
2022-08-05 10:26:26 +0700

    fixed auth test & docker-compost test

--- generators/app/templates/backend/src/api/auth/auth.test.js
+++ generators/app/templates/backend/src/api/auth/auth.test.js
@@ -2,18 +2,25 @@ import assert from 'assert';
 import supertest from 'supertest';
 import app from '../../app';
 import User from '../users/users.model';
+import authService from '../auth/auth.service';
 
 const api = supertest(app);
 
 beforeEach(async () => {
-  await User.deleteOne({ email: 'minhuyendo@gmail.com' });
+  await User.deleteOne({ email: 'test@test.com' });
+  await User.deleteOne({ email: 'testLogin@test.com' });
+  await authService.signup({
+    email: 'testLogin@test.com',
+    password: '123456',
+    fullName: "TestLogin"
+  });
 });
 
 describe('GET /login', function() {
   it('should return 200', async function() {
     await api
       .post('/api/v1/auth/login')
-      .send({ email: 'admin@gmail.com', password: 'admin@123' })
+      .send({ email: 'testLogin@test.com', password: '123456' })
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200);
@@ -25,9 +32,9 @@ describe('GET /signup', function() {
     await api
       .post('/api/v1/auth/signup')
       .send({
-        email: 'minhuyendo@gmail.com',
+        email: 'test@test.com',
         password: '123456',
-        fullName: 'Uyen Do'
+        fullName: 'Test'
       })
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
