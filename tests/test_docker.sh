0f5b232014af1affcdec20f44fc1841c4d489257
Uyen Do <minhuyendo@gmail.com>
2022-08-05 10:47:18 +0700

    update test_docker.sh script

--- tests/test_docker.sh
+++ tests/test_docker.sh
@@ -17,7 +17,7 @@ yo ../../generators/app --no-input
 
 cd awesome-express-project
 
-cp .env.exammple .env
+cp .env.example .env
 
 # run the project's tests
 docker-compose -f docker-compose.test.yml build
