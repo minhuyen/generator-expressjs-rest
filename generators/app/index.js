75a7d81b453d117f21f3d70be8cb3671825d18a0
Uyen Do <minhuyendo@gmail.com>
2022-08-05 10:01:14 +0700

    update generate options

--- generators/app/index.js
+++ generators/app/index.js
@@ -9,20 +9,18 @@ module.exports = class extends Generator {
     super(args, opts);
 
     // Next, add your custom code
-    this.option("input", { type: Boolean }); // This method adds support for a `--no-input` flag
-    this.log("--no-input", this.options["no-input"]);
-    this.noInput = this.options["no-input"] || true;
-    this.log("===noInput====", this.noInput);
+    this.option("input", { type: Boolean, default: true }); // This method adds support for a `--no-input` flag
+    this.input = this.options["input"];
   }
 
   async prompting() {
-    if (!this.noInput) {
+    if (this.input) {
       this.answers = await this.prompt([
         {
           type: "input",
           name: "name",
           message: "Your project name",
-          default: "awesome-project", // Default to current folder name
+          default: "awesome-express-project", // Default to current folder name
         },
         {
           type: "confirm",
@@ -38,7 +36,7 @@ module.exports = class extends Generator {
   writing() {
     const coolFeature = this.answers?.cool || true;
     this.log("cool feature", coolFeature); // user answer `cool` used
-    const projectName = this.answers?.name || "awesome-project";
+    const projectName = this.answers?.name || "awesome-express-project";
     this.fs.copyTpl(
       this.templatePath(),
       this.destinationPath(projectName),
