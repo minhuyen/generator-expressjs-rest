c36769d0931a0615d2dd79f9c11eeedc80ca76a1
Uyen Do <minhuyendo@gmail.com>
2024-09-12 15:33:16 +0700

    feat: deploy script

--- generators/app/templates/scripts/deploy.production.sh
+++ generators/app/templates/scripts/deploy.production.sh
@@ -3,5 +3,5 @@ cd <%=project_slug%>
 git pull
 echo ${CI_REGISTRY_PASSWORD} | docker login $CI_REGISTRY --username $CI_REGISTRY_USERNAME --password-stdin
 docker pull ${CI_REGISTRY_IMAGE}:latest
-docker-compose -f docker compose.prod.yml up -d
+docker compose -f docker-compose.prod.yml up -d
 docker image prune -f
